bb800fe51debbab61c33add0e735f7a9a809d3a4
Sebastian Prieto <sebasprietotovar@hotmail.com>
2024-02-03 12:15:55 -0500

    Added a now path to the main file

--- db/client.py
+++ db/client.py
@@ -10,10 +10,4 @@ from pymongo import MongoClient
 # Cloud database
 
 MONGO_URI = "mongodb+srv://tsting_user:testinguser@mongosebasp.ozcuobk.mongodb.net/?retryWrites=true&w=majority"
-db_client = MongoClient(
-  MONGO_URI,
-  ssl=True,
-  ssl_cert_reqs=ssl.CERT_NONE,
-  ssl_match_hostname=False,
-  ssl_purpose=ssl.Purpose.SERVER_AUTH
-).MongoSebasP
+db_client = MongoClient(MONGO_URI,).MongoSebasP
